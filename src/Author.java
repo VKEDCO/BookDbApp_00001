a7d7dad6bbcd9821f17861b3a7d8357b3828d3cd
vkedco <vladimir.kulyukin@gmail.com>
2013-10-25 13:29:34 -0700

    Create Author.java

---
+++ src/Author.java
@@ -0,0 +1,32 @@
+package org.vkedco.mobappdev.simple_db_app_01;
+
+public class Author {
+
+	protected String mName;
+	protected int mBirthYear;
+	protected int mDeathYear;
+	protected String mCountry;
+	
+	public Author(String name, int by, int dy, String country) {
+		mName = name;
+		mBirthYear = by;
+		mDeathYear = dy;
+		mCountry = country;
+	}
+	
+	public String getName() {
+		return mName;
+	}
+	
+	public int getBirthYear() {
+		return mBirthYear;
+	}
+	
+	public int getDeathYear() {
+		return mDeathYear;
+	}
+	
+	public String getCountry() {
+		return mCountry;
+	}
+}
