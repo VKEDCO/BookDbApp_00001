28878e467925b056a3e24253466f72519f1656ca
vkedco <vladimir.kulyukin@gmail.com>
2014-11-12 13:28:42 -0700

    Create Book.java

---
+++ SufiPoetryBooksDB/org.vkedco.mobappdev.sufipoetrybooksdb/Book.java
@@ -0,0 +1,41 @@
+package org.vkedco.mobappdev.sufipoetrybooksdb;
+
+public class Book {
+	protected String mTitle;
+	protected String mAuthor;
+	protected String mTranslator;
+	protected String mISBN;
+	protected float mPrice;
+	
+	public Book(String title, String author, String translator, String isbn, float price) {
+		mTitle = title;
+		mAuthor = author;
+		mTranslator = translator;
+		mISBN = isbn;
+		mPrice = price;
+	}
+	
+	public String getTitle() {
+		return mTitle;
+	}
+	
+	public String getAuthor() {
+		return mAuthor;
+	}
+	
+	public String getISBN() {
+		return mISBN;
+	}
+	
+	public String getTranslator() {
+		return mTranslator;
+	}
+	
+	public float getPrice() {
+		return mPrice;
+	}
+	
+	public String toString() {
+		return "BOOK " + mTitle + "; " + mAuthor + "; " + mTranslator + "; " + mISBN + "; " + mPrice;
+	}
+}
